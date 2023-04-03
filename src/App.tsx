8d72dbcb7df7ffc2f5d9246e7db100b2f9a8c8c8
PlanetofJay <jae44dv@gmail.com>
2023-04-03 03:42:23 -0400

    test optional value

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  const calcTotal = (value1: number, value2: number) => {
+  const calcTotal = (value1: number, value2: number, value3?: number) => {
     
     // explicitly declare the type
     let sum: number = value1 + value2;
@@ -8,10 +8,7 @@ function App() {
 
   const price = parseInt('5')
   const total: string = calcTotal(price, 6);
-  
-  let aux: (string | number)[];
-  aux = ['abc', 123, 'def', 456, 789, 'ghi'];
-  aux = [56346];
+  const total2: string = calcTotal(3, 7, 5);
 
   return (
     <div>
