786228d51f26cf688f2089975d0171792d1f4b1a
PlanetofJay <jae44dv@gmail.com>
2023-04-03 04:04:21 -0400

    test ANY

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 function App() {
-  const doubleValue = (value?: number | string) => {
+  const doubleValue = (value?: any) => {
     if (!value) {
-      return '';
+      return 'None';
     }
 
     // Validate the typeof
@@ -15,13 +15,29 @@ function App() {
     return value;
   }
 
+  let name = 'John';
+  let age = 35;
+  let poj; // any
+  poj = 'John';
+  poj = 35;
+  poj = [1, 2, 3];
+
+
   const result1 = doubleValue(8);
   const result2 = doubleValue('John');
+  const result3 = doubleValue();
+  const result4 = doubleValue(true);
+  const result5 = doubleValue([1, 2, 3]);
+  const result6 = doubleValue({ name: 'John'});
 
   return (
     <div>
       <div>First Result is {result1}</div>
       <div>Second Result is {result2}</div>
+      <div>Third Result is {result3}</div>      
+      <div>Fourth Result is {result4}</div>      
+      <div>Fifth Result is {result5}</div>      
+      <div>Sixth Result is {result6}</div>      
     </div>
   );
 }
