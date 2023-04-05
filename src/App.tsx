a1f200114b87eee84453dd07533b2d2429b675eb
PlanetofJay <jae44dv@gmail.com>
2023-04-04 20:25:25 -0400

    test 2 options

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 
 function App() {
-  const doubleValue = (value?: number | string): string => {
-    if (!value) {
-      return 'None';
-    }
 
-    // Validate the typeof
-    if (typeof value === 'string') {
-      value = value + value;
+  const getStatusMessage = (isPublished: boolean): string => {
+    if (isPublished) {
+      return 'This has been published.';
     }
     else {
-      value = value * 2;
-      value = value.toFixed(1);
+      return 'This is not published yet.'
     }
-    return value;
   }
-
-  const result1 = doubleValue(8);
-  const result2 = doubleValue('John');
-  const result3 = doubleValue();
-
-  const showAlert = (): void => {
-    alert('ok');
-  }
-  showAlert();
+  const statusMessage = getStatusMessage(true);
 
   return (
     <div>
-      <div>First Result is {result1}</div>
-      <div>Second Result is {result2}</div>
-      <div>Third Result is {result3}</div>      
+      <div>Status: {statusMessage}</div>
     </div>
   );
 }
