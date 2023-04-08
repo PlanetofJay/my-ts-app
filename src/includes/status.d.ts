d7cd7ffc13ae5c45629e0618cf9f9b26cb740fb3
PlanetofJay <jae44dv@gmail.com>
2023-04-08 02:59:08 -0400

    work with definition file

--- src/includes/status.d.ts
+++ src/includes/status.d.ts
@@ -1,2 +1,8 @@
-export const statuses: any;
-export function getStatus(id: string);
+interface IStatus {
+  id: string,
+  /** A label for the status. */
+  text: string
+}
+
+export const statuses: IStatus[];
+export function getStatus(id: string): IStatus | undefined;
